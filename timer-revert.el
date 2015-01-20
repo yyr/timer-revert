cfcaf6dfc19a7ec38218ca9e82012718717a939e
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2015-01-20 16:45:11 +0900

    * timer-revert.el: working version.

--- timer-revert.el
+++ timer-revert.el
@@ -17,29 +17,33 @@
 
 (defun timer-revert-buffer ()
   "revert buffer if not modified."
-  (let ((buf (buffer-name)))
-    (if (and (buffer-file-name)
-             (file-exists-p (buffer-file-name))
+  (let ((buf timer-revert-buffer))
+    (if (and (buffer-file-name buf)
+             (file-exists-p buf)
              (buffer-modified-p))
         (progn
           (revert-buffer t t t)
-          (message "Refreshed buffer"))
-      (message "file has not changed"))))
+          (message "%s refreshed buffer" buf))
+      (message "%s file has not changed outside" buf))))
 
-;; (setq timer-revert-timer 3)
-(defvar timer-revert-timer nil)
-(define-minor-mode timer-revert
+;;; debug
+;; (setq timer-revert-delay 3)
+;; (setq timer-revert-timer nil)
+
+(defvar-local timer-revert-buffer (current-buffer))
+(defvar-local timer-revert-timer nil)
+(define-minor-mode timer-revert-mode
   "revert buffer for every `timer-revert-delay'"
   :group timer-revert
   :init-value nil
   (and timer-revert-timer (cancel-timer timer-revert-timer))
-  (if timer-revert
+  (if timer-revert-mode
       (progn
         (setq timer-revert-timer
               (run-at-time t timer-revert-delay
                            'timer-revert-buffer)))
     (and timer-revert-timer
-         (cancel-timer timer-revert-timer))))
+         (cancel-timer  timer-revert-timer))))
 
 (provide 'timer-revert)
 ;;; timer-revert.el ends here
