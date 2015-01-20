c9eb4cdc38dcddb7da2b0429fe71b4054c58c77a
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2015-01-20 17:33:59 +0900

    * timer-revert.el: Header thingy

--- timer-revert.el
+++ timer-revert.el
@@ -1,10 +1,30 @@
 ;;; timer-revert.el --- minor mode to revert buffer for a given time interval.
+
+;; Copyright (C) 2015 Yagnesh Raghava Yakkala
+
 ;; Author: Yagnesh Raghava Yakkala. http://yagnesh.org
+;; Maintainer: hi@yagnesh.org
 ;; Created: Tuesday, January 20 2015
-;; URL: http://github.com/yyr/timer-revert
-;; Version: 0.1
 ;; Keywords: timer, revert, auto-revert.
-;;
+;; Version: 0.1
+;; URL: http://github.com/yyr/timer-revert
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 ;;; Commentary:
 ;; A minor mode to revert buffer for a given time interval.
 ;;
